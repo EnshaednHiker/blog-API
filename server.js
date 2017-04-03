c7ee9779dcf2ce83dad2305eaf63845d87ac9b54
EnshaednHiker <nbingham09@gmail.com>
2017-04-03 18:52:41 -0500

    blog API server finished and tested, all four methods (GET, PUT, POST, DELETE) work

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const blogRouter = require('./blogRouter');
 
 server.use(morgan('common'));
 
-server.use('/blog-posts', recipesRouter);
+server.use('/blog-posts', blogRouter);
 
 server.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
