1c4dbd7e03102a2042bcae497780bc868e387e72
EnshaednHiker <nbingham09@gmail.com>
2017-04-07 17:05:54 -0500

    cleaned up extraneous files

--- tests/isEqual.js
+++
@@ -1,9 +0,0 @@
-
-
-
-// I thought it was an error on thinkful's part to not end the line below with a semicolon. I did that and broke their test.
-//What gives?
-
-//what's the difference between the below with and without ending in a semicolon?
-
-module.exports = function(a, b) {return a === b}
