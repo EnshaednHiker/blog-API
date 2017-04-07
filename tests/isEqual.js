21307392cf92f92ab015fff99bcb469d031b6b3f
EnshaednHiker <nbingham09@gmail.com>
2017-04-07 17:02:16 -0500

    renamed folder to tests for semantic readability

---
+++ tests/isEqual.js
@@ -0,0 +1,9 @@
+
+
+
+// I thought it was an error on thinkful's part to not end the line below with a semicolon. I did that and broke their test.
+//What gives?
+
+//what's the difference between the below with and without ending in a semicolon?
+
+module.exports = function(a, b) {return a === b}
